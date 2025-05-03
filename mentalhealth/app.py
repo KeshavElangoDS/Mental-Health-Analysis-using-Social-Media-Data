e814fd1f80e983e564a97d7f3c653dec1bb74120
Keshav Elango <144287174+KeshavElangoDS@users.noreply.github.com>
2025-05-03 18:04:12 -0400

    update path to run in streamlitt cloud

--- mentalhealth/app.py
+++ mentalhealth/app.py
@@ -31,6 +31,8 @@ import sys
 import torch
 from PIL import Image
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 st.set_page_config(layout="wide", page_title=" Mental Health Text Classifier")
 
 st.markdown(
